b1d875b7a23504057e3fdb8fa24d229f74d7a74b
Hayden Luckenbach <hluckenb@uark.edu>
2014-04-04 15:11:08 -0500

    Remove FAQ.php

--- main/faq.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>FAQ</title>
-<link rel="stylesheet" type="text/css" href="template.css">
-</head>
-<body><center><h1>FAQ</h1></center>
-<center>Q. Are FAQ's even Helpful?</center>
-<center>A. Not ususally.</center>
-</body></html>
