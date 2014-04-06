a68479bea713c64a8aeb2e7fb26da54564a01920
pfeinberg <p.feinberg@yahoo.com>
2014-04-06 16:13:01 -0500

    Fixed Day View
    Day view now generates correct page navigation

--- main/calendar/day.php
+++ main/calendar/day.php
@@ -1,68 +1,90 @@
-<html>
-<head>
 <title>Day</title>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script src="/bootstrap/js/bootstrap.js" type="text/javascript"></script>
-</head>
 
-<body>
 <?php
 	require_once('CalendarFunctions.php');
-	$day = $_GET['d'];
-	$month = $_GET['m'];
-	$year = $_GET['y'];
 
-	$cmdays = date('t', mktime(0,0,0,$month,1,$year));
+	if(!isset($_GET['y'])){
+			$year = date('Y');
+	}
+	else{
+			$year = $_GET['y'];
+	}
+	if(!isset($_GET['m'])){
+			$month = date('m');
+	}
+	else{
+			$month = $_GET['m'];
+	}
+	if(!isset($_GET['d'])){
+			$day = date('d');
+	}
+	else{
+			$day = $_GET['d'];
+	}
+	
+	$draw = draw_day($day,$month,$year);
+	$pday = $day-1;
+	$nday = $day+1;
+	
+	$pmonth = $month;
+	$nmonth = $month;
+	
+	$pyear = $year;
+	$nyear = $year;
+	
+	if($nday > date('t', mktime(0,0,0,$month,1,$year))){
+		$nmonth = $nmonth+1;
+		$nday = 1;		
+	}
+	elseif($nday < 1)
+	{
+		$nmonth = $month-1;
+		$nday = date('t', mktime(0,0,0,$month,1,$year));
+	}
+	
+	if($pday > date('t', mktime(0,0,0,$month,1,$year))){
+		$pmonth = $month+1;
+		$pday = 1;		
+	}
+	elseif($pday < 1)
+	{
+		$pmonth = $month-1;
+		$pday = date('t', mktime(0,0,0,$month,1,$year));
+	}
+	
+	if($pmonth < 1)
+	{
+		$pyear = $year - 1;
+		$pmonth = 12;
+	}
+	elseif($pmonth > 12)
+	{
+		$pyear = $year + 1;
+		$pmonth = 1;
+	}
+	
+	if($nmonth < 1)
+	{
+		$nyear = $year - 1;
+		$nmonth = 12;
+	}
+	elseif($nmonth > 12)
+	{
+		$nyear = $year + 1;
+		$nmonth = 1;
+	}
+	
+	
+	
 
-	if(isset($_GET['tar'])){
-		if($_GET['tar'] == 'f'){
-			$day++;
-			$draw = draw_day($day-1,$month,$year);
-			if($day > $cmdays):
-				//$month = $month+1;
-				$day = 1;		
-				//$day++;
-			endif;
-			}
-			elseif($_GET['tar'] == 'b'){
-			$day--;
-			$draw = draw_day($day+1,$month,$year);
-			if($day < 1):
-				$month = $month-1;
-				$day = date('t', mktime(0,0,0,$month-1,1,$year));
-				// 	$day=;
-			endif;
-			}
-		}
+	
 
 echo '<div class="day">
 			<ul class="pager">
-			  <li><a href="/main/index.php?act=day&m='. $month .'&d='. $day .'&y='. $year .'&tar=b">Yesterday</a></li>
-			  <li><a href="/main/index.php?act=day&m='. $month .'&d='. $day .'&y='. $year .'&tar=f">Tomorrow</a></li>
-			  <br>
-			  <br>
-			  <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Create Event</button>
+			  <li><a href="/main/index.php?act=day&m='. $pmonth .'&d='. $pday .'&y='. $pyear .'">Yesterday</a></li>
+			  <li><a href="/main/index.php?act=day&m='. $nmonth .'&d='. $nday .'&y='. $nyear .'">Tomorrow</a></li>
 			</ul>
             '. 	$draw . '
           </div>';
 ?>
-
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">New Event</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Create Event</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-      </div>
-    </div>
-  </div>
-</div>
-</body>
 </html>
