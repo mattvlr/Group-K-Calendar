d9649d89911a5ca4b3a37eb12c01f652d01fe3a9
Hayden Luckenbach <hluckenb@uark.edu>
2014-04-09 14:21:30 -0500

    Upcoming events SQL data now in table on upcoming
    I feel like I kind of spaghetti coded this, but I think its still
    pretty readable. The SQL data from the upcoming events now properly
    goes into a HTML table list. I will modify it further when we get all
    of the event parameters sorted so that everything is displayed
    correctly.

--- main/upcoming.php
+++ main/upcoming.php
@@ -1,10 +1,26 @@
-
+<html>
+<head>
+<title>Upcoming Events</title>
+<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="/bootstrap/css/signin.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="template.css">
+<link href="/bootstrap/css/dashboard.css" rel="stylesheet">
+</head>
+<body><center><h1>Upcoming Events</h1>
 
 <?php
 $today = date("Y-m-d H:i:s");
 $events = $mysql->getEvents( "NULL", $today ,"day");
 $num_events = count($events);
 
+?>
+
+<br> There are <?php echo $num_events; ?> upcoming events in the next 3 days.<br> Draft with SQL data:</center>
+
+<br>
+<br>
+
+<?php
 
 if($num_events == '0')
 {
@@ -12,29 +28,51 @@ $num_events = 'no';
 }
 
 $eventlist = '';
-for($i = 0; $i < $num_events; $i++)
-{
-$eventlist .= $events[$i]["title"] . "&nbsp&nbsp&nbsp". $events[$i]["event_date"] ;
-$eventlist .= '<br>';
-}
+
 ?>
 
-<html>
-<head>
-<title>Upcoming Events</title>
-<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="/bootstrap/css/signin.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="template.css">
- <link href="/bootstrap/css/dashboard.css" rel="stylesheet">
-</head>
-<body><center><h1>Upcoming Events</h1>
-<br> There are <?php echo $num_events; ?> upcoming events in the next 3 days.<br>
-<br> <?php echo $eventlist; ?></center>
+
+<center>
+<table class="table table-striped table-hover" style="width:600px;" >
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Event Name</th>
+      <th>Date</th>
+      <th>Time</th>
+      <th>Location</th>
+      <th>Owner/Creator</th>
+    </tr>
+    </thead>
+  <tbody>
+<?php
+
+for($i = 0; $i < $num_events; $i++)
+{
+?>
+    <tr>
+      <td><?php echo $i; ?></td>
+      <td><?php echo $events[$i]["title"]; ?></td>
+      <td><?php echo $events[$i]["event_date"]; ?></td>
+      <td>8:00AM</td>
+      <td>Somewhere</td>
+      <td>Matt</td>
+    </tr>
+
+<?php 
+} 
+?>
+
+</tbody>
+</table> 
+</center>
+
+<center>
+
+Finished Example:
 
 <br>
-<br>
 
-<center>This is what I have in mind for the event list. <br>Maybe click event to go to event page with description/options etc.<br>Colors represent priority?<br>Enumerate html table with PHP elements.
 <table class="table table-striped table-hover" style="width:600px;" >
   <thead>
     <tr>
@@ -79,7 +117,7 @@ $eventlist .= '<br>';
       <td>Back there</td>
       <td>Kenny</td>
     </tr>
-    <tr class="active">
+    <tr>
       <td>5</td>
       <td>Event</td>
       <td>1/1/2014</td>
