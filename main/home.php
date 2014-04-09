79e0860f72320755fa20916b4758e63f8aa6c532
Hayden Luckenbach <hluckenb@uark.edu>
2014-04-09 16:16:30 -0500

    New logo complete.
    We big time now

--- main/home.php
+++ main/home.php
@@ -4,16 +4,16 @@
 if(isset($_SESSION['first_name']) && isset($_SESSION['last_name']))
 {
 $body = '<center><div class="jumbotron" align="left" style="padding-left:300px;">
-  <h1>Welcome '. $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] . '!</h1>
-  <p>This is Group Ks Kalendar project.</p>
+  <h1>Welcome, '. $_SESSION['first_name'] .'.</h1>
+  <p>This is Group Ks Kalendar project.<br>Group scheduling, redesigned.</p>
   <p><a class="btn btn-primary btn-lg" href="?act=month">Get started</a></p>
 </div></center>';
 }
 else
 {
 $body = '<center><div class="jumbotron" align="left" style="padding-left:300px;">
-  <h1>Welcome!</h1>
-  <p>This is Group Ks Kalendar project.</p>
+  <h1>Welcome.</h1>
+  <p>This is Group Ks Kalendar project.<br>Group scheduling, redesigned.</p>
   <p><a class="btn btn-primary btn-lg" href="?act=login">Get started</a></p>
 </div></center>';
 }
