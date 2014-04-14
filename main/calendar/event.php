159dc46468a435b9dda735a2168bac462029fd36
mattvlr <mattv.vandenberg@gmail.com>
2014-04-14 15:23:43 -0500

    Small fix to event page
    committed before class ended, so I wouldn't forget.

--- main/calendar/event.php
+++ main/calendar/event.php
@@ -1,8 +1,6 @@
-<title>Event</title><br><br><br>
-<center><div style="text-align: center; width:50%;"> This is my poorly designed event page
-if someone wants to clean it up, please do. Also I left out location on accident. -Matt <br>
+
 <?php
-		print_r($_GET); // comment out
+		//print_r($_GET); // comment out
 		$priority = $_GET['el'][0];
 		$date_created = $_GET['el'][1];
 		$day = $_GET['el'][2];
@@ -11,7 +9,10 @@ if someone wants to clean it up, please do. Also I left out location on accident
 		$title = $_GET['el'][5];
 		$description = $_GET['el'][6];
 		$time = $_GET['el'][7];
-
+		
+		echo '<title>'.$title.'</title><br><br><br>
+			  <center><div style="text-align: center; width:50%;"> This is my poorly designed event page
+              if someone wants to clean it up, please do. Also I left out location on accident. -Matt <br>';
 		echo '<br><br><h1>'.$title.'</h1>';
 		echo 'Event Date: '. $month .' '. $day .' '. $year .' at '. $time;
 		echo '<p>Description: '. $description .' </p>';
