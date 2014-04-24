b4c42ab74d4bd3a130e645ced55152fb707d8ee7
klewisjr <kkenlew4@aol.com>
2014-04-24 02:54:25 -0500

    Settings sends data
    this is why my github was crazy, someone else changed the settings
    before i committed. but anyways igot the settings page to send data,
    still not done. also, i don’t know why, but nothing worked until i
    picked a theme. for some reason, i was unable to access $id. but when i
    picked a theme it worked again

--- main/usersettings.php
+++ main/usersettings.php
@@ -2,21 +2,26 @@
 
 //GET USERS SETTINGS FROM DATABASE TO POPULATE FORM
 
+	$user = $mysql->select('user', array('first_name','last_name','info','avatar'), "id = ".$id."");
+		if($user['avatar'] != ''){$avatarlocation = $user['avatar'];}
+		if($user['info'] != ''){$info = $user['info'];}
+		$first = $user['first_name'];
+		$last = $user['last_name'];
+
 //GENERATE FORM
 
 $form = '	
-<form class="form-signin" role="form" action="' . $_SERVER['PHP_SELF'] . '?act=login" method = "post">
-			<input type="text" name = "avatar" class="form-control" placeholder="avatarlocation" required><br>
-			<input type="text" name = "first" class="form-control" placeholder="first" required><br>
-			<input type="text" name = "last" class="form-control" placeholder="last" required><br>
-			<input type="password" name = "password" class="form-control" placeholder="Password" required><br>
+<form class="form-signin" role="form" action="' . $_SERVER['PHP_SELF'] . '?act=settings" method = "post">
+			<input type="text" name = "avatar" class="form-control" placeholder="'.$avatarlocation.'" required>
+			<input type="text" name = "first" class="form-control" placeholder="'.$first.'" required>
+			<input type="text" name = "last" class="form-control" placeholder="'.$last.'" required>
+			<input type="password" name = "password" class="form-control" placeholder="Password" required>
 			<input type="password1" name = "password1" class="form-control" placeholder="Password" required>
-			<br><br>
+
 				<div class="form-group">
 		  <label for="textArea" class="col-lg-2 control-label">About</label>
 		  <div class="col-lg-10">
-			<textarea class="form-control" rows="3" id="textArea"></textarea>
-			<span class="help-block">Info about you.</span>
+			<textarea class="form-control" rows="3" name="info" id="textArea" placeholder="'.$info.'"></textarea>
 		  </div>	
 			
 			<input type="hidden" name="login" value = "true">
@@ -24,6 +29,13 @@ $form = '
 		<button class="btn btn-lg btn-primary btn-block" type="submit">Modify Settings</button>
 		<br>
 		</form>';
+		
+//AFTER FORM IS SUBMITTED
+	if (isset($_POST['avatar']) && isset($_POST['first']) && isset($_POST['last']) && isset($_POST['password']) && isset($_POST['password1']) && isset($_POST['info'])){
+		$salt = uniqid(mt_rand(0,61),true);
+		$passhash = crypt($_POST['password'],$salt);
+		$mysql->update('user'  , 'avatar="'.$_POST['avatar'].'" , first_name="'.$_POST['first'].'" , last_name="'.$_POST['last'].'" , salt="'.$salt.'" , passhash="'.$passhash.'" , info="'.$_POST['info'].'"'  ,  'id="'.$id.'"');
+	}
 
 ?>
 
